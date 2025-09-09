e6926f812e3295ec0881061eca348b5ab3c0ab40
Laba1 + dz <brolike636@gmail.com>
2025-09-09 20:30:17 +0300

    Add project files.

---
+++ Abdulin_Lab1/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace kolleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte bit1 = 1;
+            //byte bit2 = 100;
+            //sbyte bit3 = -101;
+            //sbyte bit4 = 102;
+            //short n1 = 1;
+            //short n2 = 102;
+            //ushort n3 = 1;
+            //ushort n4 = 102;
+            //Console.WriteLine("Vvedite vasch vozrast: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Vasch vozrast = {age}");
+
+            /*
+             * ПРИКТИЧЕСКОЕ ЗАДАНИЕ!!!
+             */
+            //Console.Write("Vvedite vasch ime: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Vvedite vasch vozrast: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Privet, {name}");
+            //Console.WriteLine($"Cheres mesiz i 3 dni tebe ispolnitsi - {age}");
+            /*
+             * zad1
+             */
+            //string igra = "Call of Duty!";
+            //int number = 10;
+            //float number2 = 3.14F;
+            //string bukva = "A";
+            //Console.WriteLine($"Vasha lubimai igra - {igra}");
+            //Console.WriteLine($"Vasha lubimai zifra - {number}");
+            //Console.WriteLine($"Znachenie chisla pi - {number2}");
+            //Console.WriteLine($"Vasha lubimai bukva - {bukva}");
+            /*
+             * zad2
+             */
+            //Console.WriteLine("I");
+            //Console.WriteLine("need");
+            //Console.WriteLine("more");
+            //Console.WriteLine("power!");
+            /*
+             * zad3
+             */
+            //Console.WriteLine("\"Hello There\"");
+            /*
+             * zad4
+             */
+            //Console.Write("Введите стоимость монитора: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.Write("Введите стоимость системного блока: ");
+            //int number2 = int.Parse(Console.ReadLine());
+            //Console.Write("Введите стоимость клавиатуры: ");
+            //int number3 = int.Parse(Console.ReadLine());
+            //Console.Write("Введите стоимость мыши: ");
+            //int number4 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Теперь мы имеем цену монитора - {number}, системного блока - {number2}," +
+            //    $" клавиатуры - {number3}, мыши - {number4}");
+            //int summa = number + number2 + number3 + number4;
+            //int zena = summa / 3;
+            //Console.Write($"Стоимость покупки трёх компьютеров - {zena}");
+            /*
+             * zad5
+             */
+            //Console.Write("Vvedite chislo a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Vvedite chislo b: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Vi vveli: a = {0}, b = {1}", a, b);
+            //double z = 3 * Math.Pow(a + b, 3) + (275 * Math.Pow(b, 2) - (127 * a) - 41);
+            //Console.WriteLine("Значение функции по введёным числам: {0}", z);
+            /*
+             * zad6
+             */
+            //Console.Write("Введите свое имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите свой возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"\"Привет,{name}!\"");
+            //Console.WriteLine($"\"Сейчас тебе {age} лет.\"");
+            //Console.WriteLine($"\"В следуйщем году тебе будет {age + 1}.\"");
+            /*
+             * zad7
+             */
+            //string name = "Rinat";
+            //int age = 18;
+            //string city = "Volz";
+            //Console.WriteLine("Привет,{0}! Тебе {1} лет и ты живёшь в городе{2}", name, age, city);
+            /*
+             * zad8
+             */
+            //Console.Write("Введите темпиратуру в градусах цельсия: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Мы получили тмпературу в градусах цельсия == {0}^С",number);
+            //double number2 = number * 9 / 5 + 32;
+            //Console.WriteLine("Мы получили градусы по фаренгейту == {0}^F",number2);
+
+
+
+        }
+    }
+}
---
+++ Abdulin_Lab1/Program_DZ.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * zad1
+ */
+//Console.Write("Vvedite chislo: ");
+//int n = int.Parse(Console.ReadLine());
+//Console.Write("Vvedite 2-e chislo: ");
+//int n2 = int.Parse(Console.ReadLine());
+
+//int sum = n + n2;
+//int raz = n - n2;
+//int proz = n * n2;
+//double chast = (double)n / n2;
+
+//Console.WriteLine($"Summa == {sum}");
+//Console.WriteLine($"Raznost == {raz}");
+//Console.WriteLine($"Proizvedenie == {proz}");
+//Console.WriteLine($"Chastnoe == {chast}");
+/*
+ * zad2
+ */
+//Console.Write("Vvedite vahe ime: ");
+//string? name = Console.ReadLine();
+//Console.Write("Vvedite vah vozrast: ");
+//int age = int.Parse(Console.ReadLine());
+//Console.Write("Vvedite vah lubimie izik programirovanie: ");
+//string? izik = Console.ReadLine();
+
+//Console.WriteLine("Привет, {0}!", name);
+//Console.WriteLine("Тебе {0} лет.", age);
+//Console.WriteLine("Твой любимый язык программирования - {0}.", izik);
+/*
+ * zad3
+ */
+//Console.Write("Vvedite a: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("Vvedite b: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.Write("Vvedite c: ");
+//int c = int.Parse( Console.ReadLine());
+
+//int x = 5;
+
+//double primer = (a * Math.Pow(x, 2)) + (b * x) + c;
+//Console.WriteLine($"Znachenie funkzii = {primer}");
+/*
+ * zad4
+ */
+//Console.Write("Введите ваше имя: ");
+//string? name = Console.ReadLine();
+//Console.Write("Введите ваш возраст: ");
+//int age = int.Parse(Console.ReadLine());
+//Console.Write("Введите ваш город: ");
+//string? gorod = Console.ReadLine();
+//Console.Write("Введите ваше любимое число: ");
+//int chislo = int.Parse(Console.ReadLine());
+//Console.Write("Введите темпиратуру в градусах Цельсия: ");
+//int temp = int.Parse(Console.ReadLine());
+
+//double z = Math.Pow(chislo, 2);
+//Console.WriteLine();
+//Console.WriteLine("=== ПЕРСОНАЛЬНЫЙ ПРОФИЛЬ ===");
+//Console.WriteLine();
+//Console.WriteLine($"Привет, {name}");
+//Console.WriteLine($"Тебе {age} лет.");
+//Console.WriteLine($"В следуйщем году тебе будет {age + 1}.");
+//Console.WriteLine();
+//Console.WriteLine($"Ты живёшь в городе {gorod}.");
+//Console.WriteLine($"Твоё любимое число: {chislo} (в квадрате: {z})");
+//Console.WriteLine();
+//double temp2 = temp * 9 / 5 + 32;
+//Console.WriteLine("Темпиратура за окном:");
+//Console.WriteLine("- В Цельсиях: {0}^C",temp);
+//Console.WriteLine("- В Фаренгейтах: {0}^F", temp2);
+//Console.WriteLine();
+//Console.WriteLine("=== КОНЕЦ ПРОФИЛЯ ===");
